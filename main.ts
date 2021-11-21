069f791c50d74f20de116d42c3d361419eb7434b
Daniel Komisar <dkomisar@gmail.com>
2021-11-21 17:49:26 -0500

    working

--- main.ts
+++ main.ts
@@ -47794,6 +47794,8 @@ if ( true && __webpack_require__.c[__webpack_require__.s] === module) {
 
 const Renderer = vextab.Vex.Flow.Renderer;
 const Artist = vextab.Artist;
+const VexTab = vextab.VexTab;
+
 export default class MusicPlugin extends Plugin {
 	static postprocessor: MarkdownPostProcessor = async (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
 		console.log("post processor running")
@@ -47802,11 +47804,14 @@ export default class MusicPlugin extends Plugin {
 
 	static render_vextab(el:HTMLElement) {
 			const data = `
+options space=20
 tabstave notation=true key=A time=4/4
 
 notes :q =|: (5/2.5/3.7/4) :8 7-5h6/3 ^3^ 5h6-7/5 ^3^ :q 7V/4 |
-notes :8 t12p7/4 s5s3/4 :8 3s:16:5-7/5 :q p5/4
+notes :8 t12p7/4 s5s3/4 :8 3s:16:5-7/5 :h p5/4
 text :w, |#segno, ,|, :hd, , #tr
+
+options space=25    
 `
 
 		console.log("Parsing vextab")
@@ -47822,10 +47827,10 @@ text :w, |#segno, ,|, :hd, , #tr
 		const renderer = new Renderer(blockToReplace, Renderer.Backends.SVG);
 		const artist = new Artist(10, 10, 750, { scale: 0.8 });
 
-		//const tab = new VexTab(artist);
-		//tab.parse(data);
+		const tab = new VexTab(artist);
+		tab.parse(data);
 /*  */
-		//artist.render(renderer)
+		artist.render(renderer)
 	}
 
 	async onload() {
